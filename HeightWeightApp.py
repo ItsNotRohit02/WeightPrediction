79f81a1e5ce439bb471e0f83331a4b6682dc8350
ItsNotRohit <rohit2k02@gmail.com>
2022-11-24 18:53:41 +0530

    Initial commit

--- HeightWeightApp.py
+++ HeightWeightApp.py
@@ -5,7 +5,7 @@ model=joblib.load('HeightWeightAIModel.joblib')
 
 st.title('Welcome to Weight Predictor!')
 st.header('How we predict ?')
-st.text('We use Machine Learning Algorithms to learn from data\ncollected from over 10,000 individuals of various ethinicities,\nreligions, income levels, quality of life and between the\nthe age group of 20 to 50 years to create a complex\nand accurate model to predict the Weight of an individual.')
+st.write('We use the Decision Tree Regression Algorithms to learn from data collected from over 10,000 individuals of various ethinicities, religions, income levels, quality of life and between the the age group of 20 to 50 years to create a complex and accurate model to predict the Weight of an individual.')
 st.header('Select from Options below')
 
 gender = st.radio('Select your Gender',['Male','Female'])
